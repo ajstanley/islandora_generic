fb32425d56c3bf039d0556295ef8a7bf1b6320e5
Alan Stanley <astanley@upei.ca>
2013-04-29 12:04:11 -0300

    Coder standards, itql switched to sparql

--- js/jqueryFileTree.js
+++ js/jqueryFileTree.js
@@ -33,166 +33,118 @@
 //function to parse urls
 
 
-(function($){
+(function($) {
+  Drupal.behaviors.IslandoraFileview = {
+    attach: function(context, settings) {
+      base = Drupal.settings.basePath;
+      collectionRoot = Drupal.settings.islandora_fileview.fileview_basedir;
+   
+      $.extend($.fn, {
+        fileTree: function(o, h) {
 
-  base = window.location.toString().split('/islandora_generic')[0];
+          // Defaults
+          if (!o)
+            var o = {};
+          if (o.root === undefined)
+            o.root = '/';
+          if (o.pid === undefined)
+            o.pid = collectionRoot;
+          if (o.script === undefined)
+            o.script = base + '/islandora_generic/sync';
+          if (o.folderEvent === undefined)
+            o.folderEvent = 'click';
+          if (o.expandSpeed === undefined)
+            o.expandSpeed = 500;
+          if (o.collapseSpeed === undefined)
+            o.collapseSpeed = 500;
+          if (o.expandEasing === undefined)
+            o.expandEasing = null;
+          if (o.collapseEasing === undefined)
+            o.collapseEasing = null;
+          if (o.multiFolder === undefined)
+            o.multiFolder = true;
+          if (o.loadMessage === undefined)
+            o.loadMessage = 'Loading...';
+          var urlparts = window.location.toString().split('/');
+          var collection = urlparts[urlparts.length - 1];
+          collection = collection.replace('%3A', ':');
+          if (collection.indexOf(':') !== -1) {
+            o.pid = collection;
+          }
 
-  $.ajax({
-    url:base + '/islandora_generic/filetree_setup',
-    async:false,
-    success: function(data, status, xhr) {
-      collectionRoot = data;
-    },
-    error: function() {
-      alert("AJAX call failed");
-    },
-    dataType: 'html'
-  });
+          $(this).each(function() {
 
-  $.extend($.fn, {
-    fileTree: function(o, h) {
-     
-      // Defaults
-      if( !o ) var o = {};
-      if( o.root == undefined ) o.root = '/';
-      if(o.pid == undefined) o.pid = collectionRoot;
-      if( o.script == undefined ) o.script = base + '/islandora_generic/sync';
-      if( o.folderEvent == undefined ) o.folderEvent = 'click';
-      if( o.expandSpeed == undefined ) o.expandSpeed= 500;
-      if( o.collapseSpeed == undefined ) o.collapseSpeed= 500;
-      if( o.expandEasing == undefined ) o.expandEasing = null;
-      if( o.collapseEasing == undefined ) o.collapseEasing = null;
-      if( o.multiFolder == undefined ) o.multiFolder = true;
-      if( o.loadMessage == undefined ) o.loadMessage = 'Loading...';
-      var urlparts =  window.location.toString().split('/');
-      var collection = urlparts[urlparts.length -1];
-      collection = collection.replace('%3A', ':');
-      if(collection.indexOf(':') != -1){
-        o.pid = collection;
-      }
-  
-      $(this).each( function() {
-
-        function showTree(c, t, p) {
-          $(c).addClass('wait');
-          $(".jqueryFileTree.start").remove();
-          $.post(o.script, {
-            dir: t,
-            pid: p
-          }, function(data) {
-            $(c).find('.start').html('');
-            $(c).removeClass('wait').append(data);
-            if( o.root == t ) $(c).find('UL:hidden').show(); else $(c).find('UL:hidden').slideDown({
-              duration: o.expandSpeed,
-              easing: o.expandEasing
-            });
-            bindTree(c);
-          });
-        }
-
-        function bindTree(t) {
-          $(t).find('LI A').bind(o.folderEvent, function() {
-            if( $(this).parent().hasClass('directory') ) {
-              if( $(this).parent().hasClass('collapsed') ) {
-                // Expand
-                if( !o.multiFolder ) {
-                  $(this).parent().parent().find('UL').slideUp({
-                    duration: o.collapseSpeed,
-                    easing: o.collapseEasing
+            function showTree(c, t, p) {
+              $(c).addClass('wait');
+              $(".jqueryFileTree.start").remove();
+              $.post(o.script, {
+                dir: t,
+                pid: p
+              }, function(data) {
+                $(c).find('.start').html('');
+                $(c).removeClass('wait').append(data);
+                if (o.root === t)
+                  $(c).find('UL:hidden').show();
+                else
+                  $(c).find('UL:hidden').slideDown({
+                    duration: o.expandSpeed,
+                    easing: o.expandEasing
                   });
-                  $(this).parent().parent().find('LI.directory').removeClass('expanded').addClass('collapsed');
-                }
-                $(this).parent().find('UL').remove(); // cleanup
-                showTree( $(this).parent(), escape($(this).attr('rel').match( /.*\// )), escape($(this).attr('pid')) );
-                $(this).parent().removeClass('collapsed').addClass('expanded');
-              } else {
-                // Collapse
-                $(this).parent().find('UL').slideUp({
-                  duration: o.collapseSpeed,
-                  easing: o.collapseEasing
-                });
-                $(this).parent().removeClass('expanded').addClass('collapsed');
-              }
-            } else {
-              
-              h($(this).attr('pid'));
+                bindTree(c);
+              });
             }
-            return false;
-          });
-          // Prevent A from triggering the # on non-click events
-          if( o.folderEvent.toLowerCase != 'click' ) $(t).find('LI A').bind('click', function() {
-            return false;
+
+            function bindTree(t) {
+              $(t).find('LI A').bind(o.folderEvent, function() {
+                if ($(this).parent().hasClass('directory')) {
+                  if ($(this).parent().hasClass('collapsed')) {
+                    // Expand
+                    if (!o.multiFolder) {
+                      $(this).parent().parent().find('UL').slideUp({
+                        duration: o.collapseSpeed,
+                        easing: o.collapseEasing
+                      });
+                      $(this).parent().parent().find('LI.directory').removeClass('expanded').addClass('collapsed');
+                    }
+                    $(this).parent().find('UL').remove(); // cleanup
+                    showTree($(this).parent(), escape($(this).attr('rel').match(/.*\//)), escape($(this).attr('pid')));
+                    $(this).parent().removeClass('collapsed').addClass('expanded');
+                  } else {
+                    // Collapse
+                    $(this).parent().find('UL').slideUp({
+                      duration: o.collapseSpeed,
+                      easing: o.collapseEasing
+                    });
+                    $(this).parent().removeClass('expanded').addClass('collapsed');
+                  }
+                } else {
+
+                  h($(this).attr('pid'));
+                }
+                return false;
+              });
+              // Prevent A from triggering the # on non-click events
+              if (o.folderEvent.toLowerCase != 'click')
+                $(t).find('LI A').bind('click', function() {
+                  return false;
+                });
+            }
+            // Loading message
+            $(this).html('<ul class="jqueryFileTree start"><li class="wait">' + o.loadMessage + '<li></ul>');
+            // Get the initial file list
+
+            showTree($(this), escape(o.root), escape(o.pid));
           });
         }
-        // Loading message
-        $(this).html('<ul class="jqueryFileTree start"><li class="wait">' + o.loadMessage + '<li></ul>');
-        // Get the initial file list
-     
-        showTree( $(this), escape(o.root), escape(o.pid));
+
+      });
+
+
+      $('#fileview').fileTree({
+        root: '/'
+      }, function(pid) {
+        window.open(base + '/islandora/object/' + pid);
       });
     }
-
-  });
-
-
-
-})(jQuery);
-
-
-
-(function($){
-  $(document).ready(function(){
-    $('#fileview').fileTree({
-      root: '/'
-    }, function(pid) {
-      window.open(base + '/islandora/object/' + pid);
-    });
-
-    $.contextMenu({
-      selector: '.dropbox_file_item',
-      callback: function(key, options) {
-
-        var urn = $(this).parent('div').attr('urn');
-       
-        var title = $(this).text().substring(2,100);
-        title = title.trim();
-
-        var comment_text = $(this).next('.comment_text');
-        var anno_type = comment_text.find('.comment_type').text();
-
-        if(key == 'delete'){
-          if (confirm("Permananently Delete Annotation '" + title + "'")) {
-            pb_deleteAnno(urn);
-          }
-
-        }
-
-        if(key == 'edit'){
-          $(this).addClass('annotation-opened').next().show();
-          var annotation = comment_text.find('.comment_content').text();
-          var pm = $(this).find('.comment_showhide');
-          if (pm.text() == '+ ') {
-            pm.empty().append('- ');
-            console.log(this);
-            var id = $(this).attr('id').substring(5,100);
-            var canvas = $(this).attr('canvas');
-            paint_commentAnnoTargets(this, canvas, id);
-          }
-          startEditting(title, annotation, anno_type, urn)
-        }
-      },
-      items: {
-        "edit": {
-          name: "Edit",
-          icon: "edit",
-          accesskey: "e"
-        },
-        "delete": {
-          name: "Delete annotation",
-          icon: "delete"
-        }
-
-      }
-    });
-  });
+  };
 })(jQuery);
